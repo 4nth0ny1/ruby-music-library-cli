4e4d751740048d823b74f99c0429ab24c82fc733
4anth0ny1 <acatullo4@gmail.com>
2021-01-21 09:43:37 -0600

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,42 +9,19 @@ class MusicLibraryController
         @artist = artist
     end
 
-    # def call 
-    #     puts "Welcome to your music library!"
-    #     puts "To list all of your songs, enter 'list songs'."
-    #     puts "To list all of the artists in your library, enter 'list artists'."
-    #     puts "To list all of the genres in your library, enter 'list genres'."
-    #     puts "To list all of the songs by a particular artist, enter 'list artist'."
-    #     puts "To list all of the songs of a particular genre, enter 'list genre'."
-    #     puts "To play a song, enter 'play song'."
-    #     puts "To quit, type 'exit'."
-    #     puts "What would you like to do?"
-    #     gets.chomp
-    #     if gets.chomp != "exit"
-    #         call
-    #     end
-    # end
-
     def call
         puts "Welcome to your music library!"
         input = ""
         until input == "exit"
             puts "To list all of your songs, enter 'list songs'."
-
             puts "To list all of the artists in your library, enter 'list artists'."
-
             puts "To list all of the genres in your library, enter 'list genres'."
-
             puts "To list all of the songs by a particular artist, enter 'list artist'."
-
             puts "To list all of the songs of a particular genre, enter 'list genre'."
-
             puts "To play a song, enter 'play song'."
-
             puts "To quit, type 'exit'."
-
             puts "What would you like to do?"
-
+                    
             input = gets.strip
 
             if input == "list songs"
@@ -70,7 +47,6 @@ class MusicLibraryController
             if input == "play song"
                 play_song
             end
-
         end
     end
 
